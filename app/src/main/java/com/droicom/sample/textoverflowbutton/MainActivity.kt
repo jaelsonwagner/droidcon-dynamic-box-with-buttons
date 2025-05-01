fad5619edd609543c1253a6d7eb41d5082fae2c1
Jaelson Silva <3064763+jaelsonwagne@users.noreply.github.com>
2025-05-01 15:11:12 -0300

    Uses FlowRow to manage dynamic layout.
    The original solution posted on Droidcon works if deadling only with 2 buttons.
    I wanted to manage more than 2 buttons. Using FlowRow we can achieve the expected result.
    
    1st -> "Very long text that might overflow"
    2nd -> "Another button", "One more",
    3rd -> "Another long text overflowing"

--- app/src/main/java/com/droicom/sample/textoverflowbutton/MainActivity.kt
+++ app/src/main/java/com/droicom/sample/textoverflowbutton/MainActivity.kt
@@ -5,29 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import com.droicom.sample.textoverflowbutton.data.ButtonText
 import com.droicom.sample.textoverflowbutton.data.sampleButtonsText
 import com.droicom.sample.textoverflowbutton.ui.PhonePreview
 import com.droicom.sample.textoverflowbutton.ui.TabletPreview
 import com.droicom.sample.textoverflowbutton.ui.theme.TextOverflowButtonTheme
-
 
 
 class MainActivity : ComponentActivity() {
@@ -37,73 +29,31 @@ class MainActivity : ComponentActivity() {
         setContent {
             TextOverflowButtonTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    DynamicBoxWithButton(modifier = Modifier.padding(innerPadding))
+                    FlowLayoutButtons(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
-fun DynamicBoxWithButton(modifier: Modifier = Modifier) {
-    var isOverflowing by remember { mutableStateOf(false) }
-    if (!isOverflowing) {
-        HorizontalBox(
-            buttonsText = sampleButtonsText,
-            modifier = modifier,
-            onTextOverflow = { isOverflowing = it }
-        )
-    } else {
-        VerticalBox(buttonsText = sampleButtonsText, modifier = modifier)
-    }
-}
-
-@Composable
-fun VerticalBox( buttonsText: List<ButtonText>, modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier,
-        verticalArrangement = Arrangement.spacedBy(2.dp)
+fun FlowLayoutButtons(modifier: Modifier = Modifier) {
+    FlowRow(
+        modifier = modifier.padding(4.dp),
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+        maxItemsInEachRow = Int.MAX_VALUE // Let FlowRow decide based on available space
     ) {
-        buttonsText.forEach {
+        sampleButtonsText.forEach { buttonText ->
             Button(onClick = {}) {
                 Text(
-                    text = it,
+                    text = buttonText,
                     maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
+                    overflow = TextOverflow.Ellipsis
                 )
             }
         }
-    }
-}
-
-@Composable
-fun HorizontalBox(
-    buttonsText: List<ButtonText>,
-    modifier: Modifier = Modifier,
-    onTextOverflow: (Boolean) -> Unit
-) {
-    Row(
-        modifier = modifier,
-        horizontalArrangement = Arrangement.spacedBy(2.dp)
-    ) {
-        buttonsText.forEach { OverflowAwareButton(it, onTextOverflow) }
-    }
-}
-
-@Composable
-fun OverflowAwareButton(
-    text: String,
-    onTextOverflow: (Boolean) -> Unit
-) {
-    Button(onClick = {}) {
-        Text(
-            text = text,
-            maxLines = 1,
-            overflow = TextOverflow.Ellipsis,
-            onTextLayout = { textLayoutResult ->
-                onTextOverflow(textLayoutResult.hasVisualOverflow)
-            }
-        )
     }
 }
 
@@ -111,7 +61,7 @@ fun OverflowAwareButton(
 @Composable
 fun DynamicBoxWithButtonPhonePreview() {
     TextOverflowButtonTheme {
-        DynamicBoxWithButton()
+        FlowLayoutButtons()
     }
 }
 
@@ -119,6 +69,6 @@ fun DynamicBoxWithButtonPhonePreview() {
 @Composable
 fun DynamicBoxWithButtonTabletPreview() {
     TextOverflowButtonTheme {
-        DynamicBoxWithButton()
+        FlowLayoutButtons()
     }
 }
